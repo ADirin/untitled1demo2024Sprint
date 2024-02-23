1aebb06ec32b341b43d5f74ece13ebf47c9ca43f
Amir Dirin <amir.dirin@yahoo.com>
2024-02-23 09:41:27 +0200

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int addme(int a, int b) {return a+b;}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" + addme(11,5));
+    }
+
+
+}
